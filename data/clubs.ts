3f405684028f2fbfedca9147182048aec194a459
SDBagel <sdbagel@gmail.com>
2021-11-04 11:46:43 -0700

    Add more front-facing logging/data

--- data/clubs.ts
+++ data/clubs.ts
@@ -3,6 +3,7 @@ export const clubs = [
   {
     id: "RsDUjjlJEr73Gl5DJPbFE",
     name: "National Art Honors Society",
+    short: "nahs",
     databases: [
       "814bc6c60d0a4e13bc3f8bf33c8a3117"
     ]
