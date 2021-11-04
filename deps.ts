fba91eff94a325e5508b1c0b2a792470620331d5
SDBagel <sdbagel@gmail.com>
2021-11-04 00:20:39 -0700

    Use Oak server

--- deps.ts
+++ deps.ts
@@ -1,2 +1,4 @@
 
 export { config } from './environment/environment.dev.ts';
+
+export { Application, Router, send } from 'https://deno.land/x/oak@v9.0.0/mod.ts';
--- main.ts
+++ main.ts
@@ -1,2 +1,26 @@
 
-import { config } from './deps.ts';
+import { config } from './deps.ts';  config,
+  Application,
+  Router,
+  send
+} from './deps.ts';
+
+const app = new Application();
+const router = new Router();
+router
+  .get('/api', (ctx) => {
+    ctx.response.body = 'Hello World!';
+  });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(async (ctx) => {
+  await send(ctx, ctx.request.url.pathname, {
+    root: `${Deno.cwd()}/app`,
+    index: 'index.html',
+  });
+})
+
+await app.listen({ port: 8000 });
+
