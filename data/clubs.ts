553f30f851e8e4065b741ff88a00f079a3527b53
SDBagel <sdbagel@gmail.com>
2021-11-11 14:45:40 -0800

    Update NAHS DB list

--- data/clubs.ts
+++ data/clubs.ts
@@ -5,7 +5,9 @@ export const clubs = [
     name: "National Art Honors Society",
     short: "nahs",
     databases: [
-      "814bc6c60d0a4e13bc3f8bf33c8a3117"
+      "814bc6c60d0a4e13bc3f8bf33c8a3117", // Art show
+      "23238e8d7e984544b511281ebac57fac", // Artist spotlight
+      "c229212b24fa44a0baacc3cbb6804f92", // Active members
     ]
   }
 ]
