c94a19419d24ce2a9e907e629e5ff0397cc84162
SDBagel <sdbagel@gmail.com>
2021-11-12 21:12:41 -0800

    Update clubs.ts

--- data/clubs.ts
+++ data/clubs.ts
@@ -8,6 +8,7 @@ export const clubs = [
       "814bc6c60d0a4e13bc3f8bf33c8a3117", // Art show
       "23238e8d7e984544b511281ebac57fac", // Artist spotlight
       "c229212b24fa44a0baacc3cbb6804f92", // Active members
+      "e7b1a8a9b0574274afc56ac73e513f32", // Activities archive
     ]
   }
 ]
