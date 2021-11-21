2e719861404771149535369f73c40fe4a8a47034
SDBagel <sdbagel@gmail.com>
2021-11-21 00:46:18 -0800

    Remove developer club entry

--- data/clubs.ts
+++ data/clubs.ts
@@ -10,13 +10,5 @@ export const clubs = [
       "c229212b24fa44a0baacc3cbb6804f92", // Active members
       "e7b1a8a9b0574274afc56ac73e513f32", // Activities archive
     ]
-  },
-  {
-    id: "4KlZ39DacZ8mCDhLxN6Pw",
-    name: "National Art Honors Society",
-    short: "nahs",
-    databases: [
-      "23238e8d7e984544b511281ebac57fac", // Artist spotlight
-    ]
   }
 ]
