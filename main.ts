e9f5df27b6b8f35b26e202ebff758998e793bc75
SDBagel <sdbagel@gmail.com>
2021-11-21 00:45:32 -0800

    Delete old cache upon publish

--- main.ts
+++ main.ts
@@ -12,10 +12,6 @@ import { getDatabase, queryDatabase } from './services/notion.ts';
 /* Startup - ensure needed directories exist */
 await Deno.mkdir(`./app/cache`, { recursive: true });
 await Deno.mkdir(`./app/meta`, { recursive: true });
-clubs.forEach(async club => {
-  club.databases.forEach(async (dbId: string) => {
-    await Deno.mkdir(`./app/content/${dbId}`, { recursive: true });
-  }); });
 console.log(`[EVT] Completed setup at ${new Date().toUTCString()}`);
 
 /* Application logic */
@@ -53,6 +49,9 @@ router
         pages = pages.concat(pages, flatten(next));
       }
 
+      // Remove existing cache and recreate dir (nanoid will invalidate old files)
+      await Deno.remove(`./app/content/${dbId}`, { recursive: true });
+      await Deno.mkdir(`./app/content/${dbId}`, { recursive: true });
       // Find properties with files and cache them
       for (const page of pages) {
         const keys = Object.keys(page).filter(key => page[key]?.type === 'files');
