353387c95ee613a0851a0bf10b85f4d82b629304
SDBagel <sdbagel@gmail.com>
2021-11-21 23:30:35 -0800

    Comment notion API service

--- services/notion.ts
+++ services/notion.ts
@@ -11,22 +11,23 @@ const baseOpts = {
   }
 };
 
+// Query database for pages and return a pagination result
 export async function queryDatabase(
     dbId: string, cursor?: string, after?: Date): Promise<PaginationResult> {
   // Database query options - pagination + filters
   const body = {
-    start_cursor: cursor ?? undefined,
+    start_cursor: cursor,
     filter: after ? {
       "property": "Modified",
       "last_edited_time": { "after": after.toISOString() }
-    } : undefined
-  };
+    } : undefined };
   // Make the query and return JSON response
   const res = await fetch(`${baseUrl}/databases/${dbId}/query`,
     { ...baseOpts, method: 'POST', body: JSON.stringify(body)});
   return res.json();
 }
 
+// Get database meta information from Notion
 export async function getDatabase(dbId: string): Promise<Flattenable> {
   const res = await fetch(`${baseUrl}/databases/${dbId}`, baseOpts);
   return res.json();
