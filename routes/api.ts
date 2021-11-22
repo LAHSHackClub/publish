f8fa8e728d0bee92333acd11c4de9b9ee8c39822
SDBagel <sdbagel@gmail.com>
2021-11-21 22:16:41 -0800

    Abstract API router into file

---
+++ routes/api.ts
@@ -0,0 +1,56 @@
+
+import { clubs, Router } from '../deps.ts';
+import { cachePages } from '../services/cache.ts';
+import { flatten } from '../services/flatten.ts';
+import { getDatabase, queryDatabase } from '../services/notion.ts';
+
+export const apiRouter = new Router();
+apiRouter
+  .get('/club/:id', ctx => {
+    ctx.response.headers.set('Content-Type', 'application/json');
+    ctx.response.body = clubs.find(c => c.id === ctx.params.id);
+  })
+  .post('/club/:id', async ctx => {
+    const club = clubs.find(c => c.id === ctx.params.id);
+    if (!club) {
+      ctx.response.status = 404;
+      ctx.response.body = { error: 'Club not found' };
+      return;
+    }
+    console.log(`[EVT] Publishing ${club.short} at ${new Date().toUTCString()}`);
+
+    for (const dbId of club.databases) {
+      // Perform metadata check to see if DB needs to be updated
+      const db = await getDatabase(dbId);
+      try {
+        const metaDb = JSON.parse(await Deno.readTextFile(`./app/meta/${dbId}.json`));
+        if (db.last_edited_time !== metaDb.last_edited_time) throw new Error();
+        else console.log(`[LOG] ${club.short}:${dbId} is up to date`);
+        continue;
+      } catch (e) { console.log(`[LOG] Updating ${club.short}:${dbId}`); }
+
+      // Query Notion database and flatten
+      let res = await queryDatabase(dbId);
+      let pages: any[] = flatten(res);
+      while (res.next_cursor) {
+        console.log(`[LOG] Querying ${club.short}:${dbId} for more pages`);
+        res = await queryDatabase(dbId, res.next_cursor);
+        pages.push(...flatten(res));
+      }
+
+      // Remove existing cache and recreate dir (nanoid will invalidate old files)
+      await Deno.remove(`./app/content/${dbId}`, { recursive: true });
+      await Deno.mkdir(`./app/content/${dbId}`, { recursive: true });
+      await Deno.remove(`./app/icon/${dbId}`, { recursive: true });
+      await Deno.mkdir(`./app/icon/${dbId}`, { recursive: true });
+      // Find properties with files and cache them
+      await cachePages(dbId, pages);
+
+      // Cache and save the flat response with updated URLs
+      await Deno.writeTextFile(`./app/meta/${dbId}.json`, JSON.stringify(db));
+      await Deno.writeTextFile(`./app/cache/${dbId}.json`, JSON.stringify(pages));
+    }
+    
+    console.log(`[EVT] Finished publishing ${club.short} at ${new Date().toUTCString()}`);
+    ctx.response.status = 204;
+  });
