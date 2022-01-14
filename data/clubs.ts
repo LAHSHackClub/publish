49adb8a202416d9b7d3324fbddfb40e331e1873f
SDBagel <sdbagel@gmail.com>
2022-01-13 19:04:05 -0800

    Update clubs.ts

--- data/clubs.ts
+++ data/clubs.ts
@@ -4,7 +4,10 @@ export const clubs = [
     name: "LAHS Classes",
     short: "classes",
     databases: [
-      "9ef0d1b5876b479f97530f714f126c32" // department-art
+      "9ef0d1b5876b479f97530f714f126c32", // department-art
+      "e1ed77c8a9c742489606298da0b735ab", // department-english
+      "11520001890f4148abd044d31d31f1e8", // department-math
+      "0f53e05965194b8bac763c5150359ed8", // department-science
     ]
   },
   {
