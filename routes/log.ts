935e0b4de92915fa994624ad2d5daa07b0a82917
SDBagel <sdbagel@gmail.com>
2022-05-20 21:56:41 -0700

    Add log fallback

--- routes/log.ts
+++ routes/log.ts
@@ -7,7 +7,13 @@ logRouter
   .get('/log/:id', ctx => {
     ctx.response.headers.set('Content-Type', 'application/json');
     if (!ctx.params.id) ctx.throw(400);
-    const p = persistence.getPersistence(ctx.params.id || "");
-    if (!p) ctx.response.body = { log: ['No active process. Publishing complete!'] };
-    else ctx.response.body = p;
+    try {
+      const p = persistence.getPersistence(ctx.params.id || "");
+      if (!p) ctx.response.body = { _log: ['No active process. Publishing complete!'] };
+      else ctx.response.body = p;
+    }
+    catch (e) {
+      // Error in getting Persistence means it doesn't exist, and thus no process is active
+      ctx.response.body = { _log: ['No active process. Publishing complete!'] };
+    }
   });
